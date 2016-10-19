c6e28e67b5e35394e06f8f5a086535a6c63a61cc
Christoffer <Jeni4@users.noreply.github.com>
2016-10-19 13:02:58 +0200

    Create Linux.com News - Read More redirect.user.js

---
+++ Linux.com News - Read More redirect.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Linux.com News - Read More redirect
+// @namespace    https://github.com/Jeni4/linux-com-news-read-more-link
+// @version      0.1
+// @description
+// @author       Jeni4
+// @match        https://www.linux.com/news/*
+// @grant        none
+// @updateURL
+// @downloadURL
+// @license
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    try {
+        textLink="Read more at";
+        nPage=-1;
+        for( i=0; i < document.links.length; i++ )
+            if( document.links[ i ].innerHTML.match( textLink ))
+                window.location.href=document.links[i].href;
+    }
+    catch (e) {
+        GM_log( 'www.linux.com News - Read More redirect - script exception: ' + e );
+        alert ( 'www.linux.com News - Read More redirect - script exception: ' + e );
+    }
+}
+)();
