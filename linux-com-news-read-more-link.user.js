63c2b3d5f7e20db2176ba1619d6ca3944d4fd9a6
Christoffer <Jeni4@users.noreply.github.com>
2016-10-19 13:04:16 +0200

    Update and rename Linux.com News - Read More redirect.user.js to linux-com-news-read-more-link.user.js

--- linux-com-news-read-more-link.user.js
+++ linux-com-news-read-more-link.user.js
@@ -6,8 +6,8 @@
 // @author       Jeni4
 // @match        https://www.linux.com/news/*
 // @grant        none
-// @updateURL    https://github.com/Jeni4/linux-com-news-read-more-link/raw/master/Linux.com%20News%20-%20Read%20More%20redirect.user.js
-// @downloadURL  https://github.com/Jeni4/linux-com-news-read-more-link/raw/master/Linux.com%20News%20-%20Read%20More%20redirect.user.js
+// @updateURL    https://github.com/Jeni4/linux-com-news-read-more-link/raw/master/linux-com-news-read-more-link.user.js
+// @downloadURL  https://github.com/Jeni4/linux-com-news-read-more-link/raw/master/linux-com-news-read-more-link.user.js
 // @license
 
 // ==/UserScript==
